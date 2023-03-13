ea1f30ce9d794e246395dbcdbfad4472eea412a1
UktamNimatov <uktam.nimatov@gmail.com>
2023-03-13 23:23:02 +0500

    problemN100 solved: Runtime 0ms. Beats 100%

---
+++ src/problem100/Solution.java
@@ -0,0 +1,27 @@
+package problem100;
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode q = new TreeNode(4, new TreeNode(3), new TreeNode(9));
+        TreeNode p = new TreeNode(4, new TreeNode(3), new TreeNode(9));
+        System.out.println(isSameTree(p, q));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+
+    public static boolean isSameTreeMySolution(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
